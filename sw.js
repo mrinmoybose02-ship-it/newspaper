21574dcc998a96f076b699f20c9637395315edad
mrinmoybose02-ship-it <mrinmoy.bose02@gmail.com>
2026-09-24 14:14:01 +0530

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,28 @@
+// sw.js
+self.addEventListener('install', function(e) {
+    self.skipWaiting();
+});
+
+self.addEventListener('activate', function(e) {
+    e.waitUntil(self.clients.claim());
+});
+
+// Notification Click Event
+self.addEventListener('notificationclick', function(event) {
+    event.notification.close();
+    if (event.notification.data && event.notification.data.url) {
+        event.waitUntil(
+            clients.matchAll({type: 'window'}).then(function(clientList) {
+                for (var i = 0; i < clientList.length; i++) {
+                    var client = clientList[i];
+                    if (client.url === event.notification.data.url && 'focus' in client) {
+                        return client.focus();
+                    }
+                }
+                if (clients.openWindow) {
+                    return clients.openWindow(event.notification.data.url);
+                }
+            })
+        );
+    }
+});
